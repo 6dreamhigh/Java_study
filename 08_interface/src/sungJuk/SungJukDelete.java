9d3271640e3acdc0debdfe9e22418426ef158838
황서정 <6dreamhigh@naver.com>
2023-02-15 19:16:45 +0900

    0215-hw

--- 08_interface/src/sungJuk/SungJukDelete.java
+++ 08_interface/src/sungJuk/SungJukDelete.java
@@ -1,16 +1,28 @@
 package sungJuk;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SungJukDelete implements SungJuk {
 
 	@Override
 	public void execute(ArrayList<SungJukDTO> arrayList) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("삭제할 이름 입력 : ");
+		String name = sc.next();
+		for(int i =0; i<arrayList.size();i++) {
+			if(name ==arrayList.get(i).getName()) {
+				//이름을 전부 arrayList에서 삭제한다.
+				System.out.println("삭제하였습니다");
+			}
+			else if(name !=arrayList.get(i).getName()) {
+				System.out.println("회원의 정보가 없습니다 ");
+				}
+			}
 		
-		
-	}
+		}
 
-}
+	}
 
 //6. SungJukDelete.java
 //- 이름을 입력하여 없는 이름이면 "회원의 정보가 없습니다" 출력하시오
